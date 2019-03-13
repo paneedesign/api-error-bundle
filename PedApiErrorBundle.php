659453d48693f02b8c6591d32973ceba33195939
Patrick Luca Fazzi <patrick@paneedesign.com>
2019-03-13 12:12:19 +0100

    feat: bundle main class developed

---
+++ PedApiErrorBundle.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * User: Patrick Luca Fazzi
+ * Date: 27/02/19
+ * Time: 12.49
+ */
+
+namespace PaneeDesign\ApiErrorBundle;
+
+
+final class PedApiErrorBundle
+{
+
+}
